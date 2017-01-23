2e3365a6d59a0fea97597aa5d4cf7bede658fcb1
Spandan <dhungelspandan@gmail.com>
2017-01-23 14:15:07 +0545

    All files added

---
+++ footer.php
@@ -0,0 +1,20 @@
+	<footer>
+	<div class="footer">
+				<div class="copyright">
+				  &copy; Copy right 2016 | <a href="#">Privacy </a>| <a href="#">Policy</a>
+				</div>
+				<div class="atisda">
+					 Designed by <a href="http://www.webdomus.net/">Web Domus Italia - Web Agency </a> 
+				</div>
+			</div>
+	</div>
+</div>
+<?php wp_footer(); ?>
+</footer>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/source/bootstrap-3.3.6-dist/js/jquery.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/source/js/isotope.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/source/js/myscript.js"></script> 
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/source/bootstrap-3.3.6-dist/js/jquery.1.11.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/source/bootstrap-3.3.6-dist/js/bootstrap.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Garage Bootstrap Template</title>
+	<meta name="description" content="">
+<!--
+
+Template 2079 Garage
+
+http://www.tooplate.com/view/2079-garage
+
+-->
+	<meta name="author" content="Web Domus Italia">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/source/bootstrap-3.3.6-dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/source/font-awesome-4.5.0/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/style/slider.css">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/style/mystyle.css">
+<?php wp_head(); ?>
+</head>
